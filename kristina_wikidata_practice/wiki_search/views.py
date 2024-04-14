26c58aa5f657ce144b61a826d79a9f3e64b0c5ed
root <root@Kristina>
2024-04-14 20:51:55 +0200

    practice with wikidata queries and django

---
+++ kristina_wikidata_practice/wiki_search/views.py
@@ -0,0 +1,40 @@
+from django.http import JsonResponse
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+def search(request):
+    # TODO: the search_string should not be hardcoded, but should be taken from user's input in the searchbar
+    search_string = "hagia"
+
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+
+    # TODO: add more details to the query, now it returns the title, description, longitude, latitude
+    query = f"""
+    SELECT DISTINCT ?item ?itemLabel ?description ?latitude ?longitude WHERE {{
+      SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
+      {{
+        SELECT DISTINCT ?item ?itemLabel WHERE {{
+          ?item ?p wd:Q406.
+          ?item ?p2 ?loc.
+          VALUES ?p {{wdt:P131 wdt:P361}}.
+          ?item rdfs:label ?itemLabel.
+          FILTER(LANG(?itemLabel) = "en")
+        }}
+      }}
+      OPTIONAL {{
+        ?item schema:description ?description.
+        FILTER(LANG(?description) = "en")
+        OPTIONAL {{ ?item wdt:P625 ?coordinates. }}
+        BIND(STRAFTER(STR(?coordinates), "(") AS ?coordsString)
+        BIND(STRBEFORE(?coordsString, " ") AS ?latitude)
+        BIND(STRBEFORE(STRAFTER(?coordsString, " "), ")") AS ?longitude)
+      }}
+                FILTER(contains(lcase(?itemLabel), "{search_string}")||contains(lcase(?description), "{search_string}"))
+
+    }}
+    ORDER BY DESC(COALESCE(?coordinates, ?description)) ?latitude ?longitude
+    LIMIT 100
+    """
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    return JsonResponse(results)
