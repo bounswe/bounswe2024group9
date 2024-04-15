417d7952db4466461bf97fecd3c2702c90f28ebb
Kristina Trajkovski <root@Kristina>
2024-04-15 13:25:01 +0200

    added two top_5 queries

--- kristina_wikidata_practice/wiki_search/urls.py
+++ kristina_wikidata_practice/wiki_search/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import search
+from .views import search, top_5_nearby, top_5_period
 
 urlpatterns = [
     path('search/', search, name='search'),
+    path('top_5_nearby/', top_5_nearby, name='top_5_nearby'),
+    path('top_5_period/', top_5_period, name='top_5_period'),
+
 ]
--- kristina_wikidata_practice/wiki_search/views.py
+++ kristina_wikidata_practice/wiki_search/views.py
@@ -8,34 +8,134 @@ def search(request):
     sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 
     # TODO: add more details to the query, now it returns the title, description, longitude, latitude
-    search_string = "galata"
-
     query = f"""
-    SELECT DISTINCT ?item ?itemLabel ?description ?latitude ?longitude WHERE {{
-      SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
-      {{
-        SELECT DISTINCT ?item ?itemLabel WHERE {{
-          ?item p:P131 ?statement0.
-          ?statement0 (ps:P131/(wdt:P131*)) wd:Q406.
-          ?item rdfs:label ?itemLabel.
-          FILTER(LANG(?itemLabel) = "en")
-        }}
-      }}
-      OPTIONAL {{
-        ?item schema:description ?description.
-        FILTER(LANG(?description) = "en")
-        OPTIONAL {{ ?item wdt:P625 ?coordinates. }}
-        BIND(STRAFTER(STR(?coordinates), "(") AS ?coordsString)
-        BIND(STRBEFORE(?coordsString, " ") AS ?latitude)
-        BIND(STRBEFORE(STRAFTER(?coordsString, " "), ")") AS ?longitude)
-      }}
-      FILTER(contains(lcase(?itemLabel), "{search_string}") || contains(lcase(?description), "{search_string}"))
+SELECT DISTINCT ?item ?itemLabel ?description ?latitude ?longitude (YEAR(?inception) AS ?inceptionYear) ?styleLabel WHERE {{
+  SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
+  {{
+    SELECT DISTINCT ?item ?itemLabel WHERE {{
+      ?item p:P131 ?statement0.
+      ?statement0 (ps:P131/(wdt:P131*)) wd:Q406.
+      ?item rdfs:label ?itemLabel.
+      FILTER(LANG(?itemLabel) = "en")
     }}
-    ORDER BY DESC(COALESCE(?coordinates, ?description)) ?latitude ?longitude
-    LIMIT 100
-    """
+  }}
+  OPTIONAL {{
+    ?item schema:description ?description.
+    FILTER(LANG(?description) = "en")
+  }}
+  OPTIONAL {{ 
+    ?item wdt:P625 ?coordinates.
+  }}
+  BIND(IF(BOUND(?coordinates), STRAFTER(STR(?coordinates), "("), "") AS ?coordsString)
+  BIND(IF(BOUND(?coordsString), STRBEFORE(?coordsString, " "), "") AS ?latitude)
+  BIND(IF(BOUND(?latitude), STRBEFORE(STRAFTER(?coordsString, " "), ")"), "") AS ?longitude)
+  OPTIONAL {{ 
+    ?item wdt:P571 ?inception. 
+  }}
+  OPTIONAL {{ 
+    ?item wdt:P149 ?style .
+    SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en" }}
+    ?style rdfs:label ?styleLabel.
+    FILTER(LANG(?styleLabel) = "en")
+  }}
+  FILTER(contains(lcase(?itemLabel), "{search_string}") || contains(lcase(?description), "{search_string}"))
+}}
+ORDER BY DESC(COALESCE(?coordinates, ?description)) ?latitude ?longitude
+"""
 
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
+    return JsonResponse(results)
+
+
+def top_5_nearby(request):
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+    longitude = 41.008333
+    latitude = 28.98
+    query = f"""
+SELECT DISTINCT ?item ?itemLabel ?description ?latitude ?longitude (YEAR(?inception) AS ?inceptionYear) (STR(?distance) AS ?distanceString) WHERE {{
+  SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
+  {{
+    SELECT DISTINCT ?item ?itemLabel WHERE {{
+      ?item p:P131 ?statement0.
+      ?statement0 (ps:P131/(wdt:P131*)) wd:Q406.
+      ?item rdfs:label ?itemLabel.
+      FILTER(LANG(?itemLabel) = "en")
+    }}
+  }}
+  OPTIONAL {{
+    ?item schema:description ?description.
+    FILTER(LANG(?description) = "en")
+    OPTIONAL {{ ?item wdt:P625 ?coordinates. }}
+    BIND(STRAFTER(STR(?coordinates), "(") AS ?coordsString)
+    BIND(STRBEFORE(?coordsString, " ") AS ?latString)
+    BIND(STRBEFORE(STRAFTER(?coordsString, " "), ")") AS ?longString)
+    BIND(xsd:float(?latString) AS ?latitude)
+    BIND(xsd:float(?longString) AS ?longitude)
+    OPTIONAL {{ ?item wdt:P571 ?inception. }}
+    BIND(abs(((?longitude - {longitude})*(?longitude - {longitude}) + (?latitude - {latitude})*(?latitude - {latitude}))) AS ?distanceSquared)
+    BIND(?distanceSquared AS ?distance)
+  }}
+  FILTER(BOUND(?distance) && ?distance > 1e-10)
+}}
+ORDER BY ?distance
+LIMIT 5
+"""
+
+
+
+    # Set the query string and format
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+
+    # Execute the query and parse the results
+    results = sparql.query().convert()
+
+    return JsonResponse(results)
+
+
+
+def top_5_period(request):
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+    inception = 532
+    
+    query = f"""
+SELECT DISTINCT ?item ?itemLabel ?description ?latitude ?longitude (YEAR(?inception) AS ?inceptionYear) WHERE {{
+  SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
+  {{
+    SELECT DISTINCT ?item ?itemLabel WHERE {{
+      ?item p:P131 ?statement0.
+      ?statement0 (ps:P131/(wdt:P131*)) wd:Q406.
+      ?item rdfs:label ?itemLabel.
+      FILTER(LANG(?itemLabel) = "en")
+    }}
+  }}
+  OPTIONAL {{
+    ?item schema:description ?description.
+    FILTER(LANG(?description) = "en")
+    OPTIONAL {{ ?item wdt:P625 ?coordinates. }}
+    BIND(STRAFTER(STR(?coordinates), "(") AS ?coordsString)
+    BIND(STRBEFORE(?coordsString, " ") AS ?latString)
+    BIND(STRBEFORE(STRAFTER(?coordsString, " "), ")") AS ?longString)
+    BIND(xsd:float(?latString) AS ?latitude)
+    BIND(xsd:float(?longString) AS ?longitude)
+    OPTIONAL {{ ?item wdt:P571 ?inception. }}
+    BIND(abs(xsd:integer(YEAR(?inception)) - {inception}) AS ?period)
+  }}
+  FILTER(BOUND(?period))
+}}
+ORDER BY ?period
+LIMIT 5
+OFFSET 1
+"""
+
+
+    # Set the query string and format
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+
+    # Execute the query and parse the results
+    results = sparql.query().convert()
+
     return JsonResponse(results)
