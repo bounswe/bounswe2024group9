c2488a42206e751b73a77d6e74b33ef6ca7f62db
root <root@Kristina>
2024-04-15 02:25:05 +0200

    broader wiki database

--- kristina_wikidata_practice/wiki_search/views.py
+++ kristina_wikidata_practice/wiki_search/views.py
@@ -8,14 +8,15 @@ def search(request):
     sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 
     # TODO: add more details to the query, now it returns the title, description, longitude, latitude
+    search_string = "galata"
+
     query = f"""
     SELECT DISTINCT ?item ?itemLabel ?description ?latitude ?longitude WHERE {{
       SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
       {{
         SELECT DISTINCT ?item ?itemLabel WHERE {{
-          ?item ?p wd:Q406.
-          ?item ?p2 ?loc.
-          VALUES ?p {{wdt:P131 wdt:P361}}.
+          ?item p:P131 ?statement0.
+          ?statement0 (ps:P131/(wdt:P131*)) wd:Q406.
           ?item rdfs:label ?itemLabel.
           FILTER(LANG(?itemLabel) = "en")
         }}
@@ -28,12 +29,12 @@ def search(request):
         BIND(STRBEFORE(?coordsString, " ") AS ?latitude)
         BIND(STRBEFORE(STRAFTER(?coordsString, " "), ")") AS ?longitude)
       }}
-                FILTER(contains(lcase(?itemLabel), "{search_string}")||contains(lcase(?description), "{search_string}"))
-
+      FILTER(contains(lcase(?itemLabel), "{search_string}") || contains(lcase(?description), "{search_string}"))
     }}
     ORDER BY DESC(COALESCE(?coordinates, ?description)) ?latitude ?longitude
     LIMIT 100
     """
+
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
