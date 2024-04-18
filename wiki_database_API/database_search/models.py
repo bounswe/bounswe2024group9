233b46bea686a36ef5512ed82bfe5fcfbaa51bb6
Mustafa (Mutti) Atak <75087023+Mutti499@users.noreply.github.com>
2024-04-19 02:26:29 +0300

    coordinates added for the Node field

--- wiki_database_API/database_search/models.py
+++ wiki_database_API/database_search/models.py
@@ -7,6 +7,9 @@ class Node(models.Model):
     name = models.CharField(max_length=255)
     node_id = models.AutoField(primary_key=True)
     photo = models.URLField()
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, default=40.984396)     # max_digits=9 and decimal_places=6 means that your latitude and longitude values will have 6 digits after the decimal point and a total of 9 digits. 
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, default=29.025434)     # This is a common choice for storing coordinates because it allows for precision up to 0.000001 degrees, which is approximately up to 11.1 centimeters at the equator.
+    # Default latitude and longitude is set to the center of Kadikoy/Istanbul
 
     def __str__(self):
         return self.name
@@ -21,7 +24,7 @@ class Route(models.Model):
     comments = models.JSONField(default=list, blank=True) 
     saves = models.PositiveIntegerField(default=0)
     nodes = models.ManyToManyField(Node, related_name='routes')
-    duration = models.JSONField(default=list, blank=True)  # Time spent in the location
+    duration = models.JSONField(default=list, blank=True)  # Time spent in the location. Type: minutes
     duration_between = models.JSONField(default=list, blank=True)  #Time spent between locations like 15 min between node 1 and 2
     mapView = models.URLField()
 
