730e764a1e8197d4a14ca414a79cc7d2e2b00556
Eray Eroğlu <erayeroglu07@gmail.com>
2024-04-20 18:55:27 +0300

    react app and design addition for serach result

---
+++ buroute-web/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import SearchResults from './SearchResults';
+
+export default function App() {
+  return (
+    <div className="App">
+      <SearchResults />
+    </div>
+  );
+}
---
+++ buroute-web/src/SearchResults.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./style.css";
+
+function SearchResults() {
+  const [searchValue, setSearchValue] = useState("");
+  const [query, setQuery] = useState('');
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      setSearchValue(event.target.value);
+      console.log(searchValue);
+      event.target.value = "";  // clear the search bar
+    }
+  };
+
+  const handleSubmit = async (event) => {
+  event.preventDefault();
+  const response = await fetch(`http://127.0.0.1:8000/wiki_search/search/${searchValue}`);
+  const data = await response.json();
+  console.log(data);
+};
+
+  return (
+  <>
+    <header>
+      <div className="search-bar">
+        <form onSubmit={handleSubmit}>
+          <input
+            id="search"
+            type="search"
+            placeholder="&#x1F50D; Start typing to search..."
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            onKeyPress={handleKeyPress}
+          />
+          <input type="submit" style={{display: 'none'}} />
+        </form>
+      </div>
+    </header>
+    <main className="container">
+      <div className="search-display"></div>
+      <div className="posts-container"></div>
+    </main>
+  </>
+);
+} export default SearchResults;
