3a4e2c271337582c228089e3d501db815af5cba1
Mustafa (Mutti) Atak <75087023+Mutti499@users.noreply.github.com>
2024-04-20 18:13:37 +0300

    add heroku app

--- wiki_database_API/wikidata_django_practice/settings.py
+++ wiki_database_API/wikidata_django_practice/settings.py
@@ -31,7 +31,9 @@ DEBUG = True
 AUTH_USER_MODEL = 'database_search.User'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    config('HEROKU_APP')
+]
 
 
 # Application definition
@@ -80,6 +82,17 @@ WSGI_APPLICATION = 'wikidata_django_practice.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+# DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.mysql',
+#       'NAME': config('DB_NAME'),
+#       'USER': config('DB_USER'),
+#       'PASSWORD': config('DB_PASSWORD'),
+#       'HOST': config('DB_HOST'),
+#       'PORT': config('DB_PORT'),
+#    }
+# }
 
 DATABASES = {
    'default': {
