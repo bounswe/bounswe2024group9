f55a7f6057de245eb4b8c721c0f18b1e2314a633
Mustafa (Mutti) Atak <75087023+Mutti499@users.noreply.github.com>
2024-04-20 17:23:56 +0300

    Heroku setup

--- wiki_database_API/wikidata_django_practice/settings.py
+++ wiki_database_API/wikidata_django_practice/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o#q#%fv)0j(&o139+(#krls8f^_8jtn!=h5(mmkux7eqy!($d='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
