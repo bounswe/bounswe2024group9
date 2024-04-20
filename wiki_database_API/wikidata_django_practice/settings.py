69be7bad6b5155f22efb5c6bfdff2c3e963e3030
Mustafa (Mutti) Atak <75087023+Mutti499@users.noreply.github.com>
2024-04-20 17:41:09 +0300

    "Set STATIC_ROOT for collectstatic"

--- wiki_database_API/wikidata_django_practice/settings.py
+++ wiki_database_API/wikidata_django_practice/settings.py
@@ -15,6 +15,7 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Quick-start development settings - unsuitable for production
