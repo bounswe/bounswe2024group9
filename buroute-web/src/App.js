d06a65e4e6e411f7dd55536fc1a39374ccccef81
Mustafa (Mutti) Atak <75087023+Mutti499@users.noreply.github.com>
2024-04-24 02:18:17 +0300

    Basic search result page and router implemented.

--- buroute-web/src/App.js
+++ buroute-web/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import React from 'react';
 import SearchResults from './SearchResults';
+import SearchDetails from './SearchDetails';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
-    <div className="App">
-      <SearchResults />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/search" element={<SearchResults />} />
+        <Route path="/result/:qid" element={<SearchDetails />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
