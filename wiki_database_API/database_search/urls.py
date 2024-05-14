0f5c9979aef6811105a533bd11c2960f7bf60ac8
Kristina Trajkovski <trajkovskikristina@gmail.com>
2024-05-14 02:52:54 +0200

    implemented route deletion back end

--- wiki_database_API/database_search/urls.py
+++ wiki_database_API/database_search/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('create_route/', views.create_route, name = 'create_route'),
     path('login/', views.login_user, name='login_user'),
     path('logout/', views.logout_user, name='logout_user'),
+    path('delete_route/', views.delete_route, name = 'delete_route')
 ]
