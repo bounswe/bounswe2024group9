17f9a0c2539fd940535cb6117b106c947c3854e1
Mutti <75087023+Mutti499@users.noreply.github.com>
2024-05-16 23:49:56 +0300

    remore pymysql

--- wiki_database_API/wikidata_django_practice/settings.py
+++ wiki_database_API/wikidata_django_practice/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
