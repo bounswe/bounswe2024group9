87c79a9831b98d74e2d0a4cb51d5a81020e3e98f
Mutti <75087023+Mutti499@users.noreply.github.com>
2024-05-17 15:27:19 +0300

    try 1

--- wiki_database_API/wikidata_django_practice/settings.py
+++ wiki_database_API/wikidata_django_practice/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 # import pymysql
 # pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- wiki_database_API/wikidata_django_practice/urls.py
+++ wiki_database_API/wikidata_django_practice/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import index 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('wiki_search/', include('wiki_search.urls')),
     path('database_search/', include('database_search.urls')),
     path('', index, name='index'), 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
