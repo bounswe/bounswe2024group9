616877f322a4de35110c1d4781987cc6157055bd
Kristina Trajkovski <trajkovskikristina@gmail.com>
2024-10-16 19:14:48 +0100

    add wiki_result view

--- django_project_491/django_app/views.py
+++ django_project_491/django_app/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, redirect
 from urllib.parse import quote
 
 # Used for initial search - returns 5 best matching wiki id's
-def search(request, search_strings):
+def wiki_search(request, search_strings):
     sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
     
     search_terms = search_strings.split() # split into words
@@ -40,6 +40,71 @@ def search(request, search_strings):
     results = sparql.query().convert()
 
     return JsonResponse(results)
+
+def wiki_result(response, wiki_id):
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+
+    # First query to get the main language information
+    query_main_info = f"""
+      SELECT ?language ?languageLabel ?wikipediaLink ?influencedByLabel ?publicationDate ?inceptionDate ?website
+      WHERE {{
+        BIND(wd:{wiki_id} AS ?language)
+
+        # Get the label of the language in English
+        ?language rdfs:label ?languageLabel.
+        FILTER(LANG(?languageLabel) = "en")
+
+        OPTIONAL {{ ?language wdt:P737 ?influencedBy. }}
+        OPTIONAL {{ ?language wdt:P577 ?publicationDate. }}
+        OPTIONAL {{ ?language wdt:P571 ?inceptionDate. }}
+        OPTIONAL {{ ?language wdt:P856 ?website. }}
+        OPTIONAL {{
+            ?wikipediaLink schema:about ?language;
+            schema:isPartOf <https://en.wikipedia.org/>.
+        }}
+
+        SERVICE wikibase:label {{ 
+          bd:serviceParam wikibase:language "en". 
+        }}
+      }}
+      LIMIT 1
+    """
+
+    sparql.setQuery(query_main_info)
+    sparql.setReturnFormat(JSON)
+    main_info_results = sparql.query().convert()
+
+    # Extract the language ID
+    language_id = f"wd:{wiki_id}"
+
+    # Second query to get all instances of the language, excluding "programming language"
+    query_instances = f"""
+      SELECT ?instance ?instanceLabel
+      WHERE {{
+        BIND({language_id} AS ?language)
+
+        # Get all instances of the language, excluding programming languages
+        ?language wdt:P31 ?instance.
+        OPTIONAL {{ ?instance rdfs:label ?instanceLabel. FILTER(LANG(?instanceLabel) = "en") }}
+        FILTER(?instance != wd:Q9143)  # Exclude programming language (wd:Q9143)
+      }}
+    """
+
+    sparql.setQuery(query_instances)
+    sparql.setReturnFormat(JSON)
+    instances_results = sparql.query().convert()
+
+    # Process the results to combine main info and instances
+    instances = [result['instanceLabel']['value'] for result in instances_results['results']['bindings'] if 'instanceLabel' in result]
+
+    # Prepare the final response
+    final_response = {
+        'mainInfo': main_info_results['results']['bindings'],
+        'instances': instances
+    }
+
+    return JsonResponse(final_response)
+
 
 
 def wikidata_query_view(request):
