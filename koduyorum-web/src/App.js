a6681cedc2c81be0ea13113336eb5399226b8d6e
ceylin <110239708+ceylingebes@users.noreply.github.com>
2024-10-18 14:52:04 +0300

    created initial version of Login/Signup/Forgot/Feed

--- koduyorum-web/src/App.js
+++ koduyorum-web/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider, useAuth } from './hooks/AuthProvider';
+import Signup from './Signup';
+import Login from './Login';
+import ForgotPassword from './Forgot';
+import Feed from './Feed';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthProvider> 
+        <AppRoutes />
+      </AuthProvider>
+    </Router>
   );
-}
+};
+
+const AppRoutes = () => {
+  const { user, loading } = useAuth();
+  console.log("User state:", user);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/forgot" element={<ForgotPassword />} />
+      {user ? (
+        <>
+          <Route path="/feed" element={<Feed />} />
+        </>
+      ) : (
+        <Route path="/" element={<Navigate replace to="/login"/>} />
+      )}
+      <Route path="*" element={<Navigate replace to="/login" state={{ from: 'private' }} />} />
+    </Routes>
+  );
+};
+
 
 export default App;
---
+++ koduyorum-web/src/Signup.js
@@ -0,0 +1,190 @@
+import React, { useState } from "react";
+import "./login_signup_style.css";
+import { useAuth } from "./hooks/AuthProvider";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [kvkk, setKvkk] = useState(false);
+  const auth = useAuth();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    if (password !== confirmPassword) {
+      setError("Password and confirm password do not match");
+      setLoading(false);
+      return;
+    }
+
+    // Block of meeting password requirements
+    if (password.match(/\d+/) === null) {
+      setError("Password must contain at least one number.");
+      setLoading(false);
+      return;
+    }
+    if (password.match(/[A-Z]/) === null) {
+      setError("Password must contain at least one upper case letter.");
+      setLoading(false);
+      return;
+    }
+    if (password.match(/[a-z]/) === null) {
+      setError("Password must contain at least one lower case letter.");
+      setLoading(false);
+      return;
+    }
+    if (password.length < 8 || password.length > 16) {
+      setError("Password must be 8 to 16 characters long.");
+      setLoading(false);
+      return;
+    }
+
+    // valid email format
+    if (
+      email.match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      ) === null
+    ) {
+      setError("Please enter a valid email.");
+      setLoading(false);
+      return;
+    }
+
+    // username requirements
+    if (!username.match(/^[0-9a-zA-Z]+$/)) {
+      setError("Username must only consist of alphanumerical characters.");
+      setLoading(false);
+      return;
+    }
+
+    if (username.length < 5 || username.length > 16) {
+      setError("Username must be 5 to 16 characters long.");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/django_app/create_user/`, // TODO: Update the API URL
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: username,
+            email: email,
+            password: password,
+          }),
+        }
+      );
+
+      const data = await response.json();
+      if (response.ok) {
+        console.log(data);
+        auth.login(data);
+
+        window.location.href = "/feed";
+      } else {
+        setError(data.error || "An error occurred");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setError("An error occurred. Please try again later.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="wrapper_entrance">
+      <div className="container_center">
+        <h2>
+          Sign Up
+        </h2>
+        {error && <div className="error-message">{error}</div>}
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="confirmPassword">Confirm password:</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+          <div className="form-group kvkk-container">
+            <input
+              type="checkbox"
+              id="kvkk"
+              name="kvkk"
+              value={kvkk}
+              onChange={(e) => setKvkk(e.target.checked)}
+            />
+            <label htmlFor="kvkk">
+              I agree to the{" "}
+              <a
+                href="https://www.resmigazete.gov.tr/eskiler/2018/03/20180310-5.htm"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                KVKK terms
+              </a>
+            </label>
+          </div>
+          {loading ? (
+            <button type="submit" className="login-button loading" disabled>
+              Loading...
+            </button>
+          ) : (
+            <button type="submit" id="signup-button" disabled={!kvkk}>
+              Sign Up
+            </button>
+          )}
+        </form>
+        <div className="signin-redirect">
+          Already have an account? <a href="/login">Sign in now</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
