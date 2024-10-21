da53cc2b74f53cf18ea82417cf9b7b8caac72b49
ceylin <110239708+ceylingebes@users.noreply.github.com>
2024-10-21 14:25:21 +0300

    update cors settings

--- django_project_491/django_project_491/settings.py
+++ django_project_491/django_project_491/settings.py
@@ -31,6 +31,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Example for React running on port 3000
+]
+
 
 # Application definition
 
@@ -42,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_app',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -52,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'django_project_491.urls'
