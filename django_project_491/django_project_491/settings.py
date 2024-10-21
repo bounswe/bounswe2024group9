cf3b038a51edd906f47934f979680b72b468cc74
Mustafa (Mutti) Atak <75087023+Mutti499@users.noreply.github.com>
2024-10-21 21:56:06 +0300

    update settings in order to allow everyone

--- django_project_491/django_project_491/settings.py
+++ django_project_491/django_project_491/settings.py
@@ -29,14 +29,15 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 ALLOWED_HOSTS = [
     os.getenv('HEOKU_APP', ''),
     '127.0.0.1',
     '10.0.2.2',
     'localhost',
-    'clownfish-app-brdp5.ondigitalocean.app',  # Add your backend domain here
-    'oyster-app-8k7np.ondigitalocean.app'    
+    'https://clownfish-app-brdp5.ondigitalocean.app',  # Add your backend domain here
+    'https://oyster-app-8k7np.ondigitalocean.app'    
 ]
 
 CORS_ALLOWED_ORIGINS = [
