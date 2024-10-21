c60c5da63c712ef07ae9bbfe92bd7198104d282d
Mutti <75087023+mutti499@users.noreply.github.com>
2024-10-21 20:12:53 +0300

    update url

--- koduyorum-web/src/Login.js
+++ koduyorum-web/src/Login.js
@@ -21,7 +21,7 @@ export const Login = () => {
     setLoading(true); // Show loading indicator
 
     try {
-      const response = await fetch(`http://127.0.0.1:8000/login/`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/login/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
