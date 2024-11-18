dbdc9c718e8442d75ef1a76b86eb1cc6bb59572b
Mustafa (Mutti) Atak <75087023+Mutti499@users.noreply.github.com>
2024-11-18 22:42:21 +0300

    fic returning a question

--- django_project_491/django_app/views/question_views.py
+++ django_project_491/django_app/views/question_views.py
@@ -28,9 +28,9 @@ def get_question_details(request: HttpRequest, question_id: int) -> HttpResponse
             'answered': question.answered,
             'topic': question.topic,
             'reported_by': [user.username for user in question.reported_by.all()],
-            'upvoted_by': [user.username for user in question.votes.filter(vote_type=VoteType.UPVOTE.value)],
-            'downvoted_by': [user.username for user in question.votes.filter(vote_type=VoteType.DOWNVOTE.value)]
-        }
+            'upvoted_by': [vote.user.username for vote in question.votes.filter(vote_type=VoteType.UPVOTE.value)],
+            'downvoted_by': [vote.user.username for vote in question.votes.filter(vote_type=VoteType.DOWNVOTE.value)]
+        }            
 
         return JsonResponse({'question': question_data}, status=200)
 
@@ -51,8 +51,8 @@ def get_question_comments(request, question_id):
             'code_snippet': comment.code_snippet,
             'language': comment.language,
             'creationDate': comment.created_at.strftime('%Y-%m-%d %H:%M:%S'),
-            'upvoted_by': [user.username for user in comment.votes.filter(vote_type=VoteType.UPVOTE.value)],
-            'downvoted_by': [user.username for user in comment.votes.filter(vote_type=VoteType.DOWNVOTE.value)],
+            'upvoted_by': [vote.user.username for vote in comment.votes.filter(vote_type=VoteType.UPVOTE.value)],
+            'downvoted_by': [vote.user.username for vote in comment.votes.filter(vote_type=VoteType.DOWNVOTE.value)],
             'answer_of_the_question': comment.answer_of_the_question
         } for comment in comments]
 
