bc84909fee1825f6f14f4e7e96f5b1bb88036c56
Mustafa (Mutti) Atak <75087023+Mutti499@users.noreply.github.com>
2024-11-18 22:40:08 +0300

    fix the upvote logic and returniong question logic

--- koduyorum-web/src/PostPreview.js
+++ koduyorum-web/src/PostPreview.js
@@ -41,7 +41,6 @@ const PostPreview = ({ post, currentUser, onClick }) => {
                 headers: {
                     'Content-Type': 'application/json',
                     'User-ID': localStorage.getItem('user_id'),
-                    'Authorization': 'Bearer ' + localStorage.getItem('authToken'),
                 },
             });
     
