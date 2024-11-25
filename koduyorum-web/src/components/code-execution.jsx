cf554a29fa0cc19cbd72129dd47810f6e969dfb1
Fatih Akgöz <44064984+FatihAkgoz@users.noreply.github.com>
2024-11-25 04:36:21 +0300

    fix create comment popup

--- koduyorum-web/src/components/code-execution.jsx
+++ koduyorum-web/src/components/code-execution.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import * as PropTypes from "prop-types";
 import { Separator } from "./ui/seperator";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import language from "react-syntax-highlighter/dist/esm/languages/hljs/1c";
-import { Navbar, LeftSidebar, RightSidebar } from '../PageComponents'; 
+import { Navbar, LeftSidebar, RightSidebar } from '../PageComponents';
 import { LoadingComponent } from '../LoadingPage';
 import PostComment from "../PostComment";
 import QuestionDetail from "../QuestionDetail";
@@ -18,7 +18,7 @@ export default function CodeExecution() {
   const { question_id } = useParams(); // Extract the questionId from the URL
   const [questionData, setQuestionData] = useState("");
   const [commentData, setCommentData] = useState("");
-  const [isAnswered,setAnswered] = useState(false);
+  const [isAnswered, setAnswered] = useState(false);
 
   const [code, setCode] = useState(""); // State to store the user input (code)
   const [output, setOutput] = useState(""); // State to store the backend's response (output)
@@ -28,6 +28,27 @@ export default function CodeExecution() {
   const [error, setError] = useState(null);
   const [languageId, setLanguageId] = useState(""); // State to store selected language ID
   const [languages, setLanguages] = useState({}); // State to store languages
+
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const openPopup = () => setIsPopupVisible(true);
+  const closePopup = () => setIsPopupVisible(false);
+  const popupRef = useRef(null);
+  useEffect(() => {
+    const handleClickOutside = event => {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        setIsPopupVisible(false);
+      }
+    };
+
+    if (isPopupVisible) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isPopupVisible]);
 
   const fetchQuestion = async () => {
     try {
@@ -122,17 +143,21 @@ export default function CodeExecution() {
       await fetchComments();
       await fetchLanguages();
     } catch (error) {
-        console.error("Error fetching initial data:", error);
+      console.error("Error fetching initial data:", error);
     } finally {
-        setIsLoading(false);
+      setIsLoading(false);
     }
   }
   // Fetch languages from the backend
   useEffect(() => {
 
-    fetchInitialData();   
+    fetchInitialData();
 
   }, []);
+
+
+  // Functions to open and close the popup
+
 
   // Function to handle form submission and send the code to the backend
   const handleSubmit = async (e) => {
@@ -162,134 +187,141 @@ export default function CodeExecution() {
   // TODO Now there is only one output box, we need to create a separate output box for each code execution
   // TODO Need to check if the comment and the question has code_snippet, if there is not there shouldnt be a run code button
   return (<div>
-    
+
     {isloading ? (
-    <LoadingComponent /> // Show loading screen while data is being fetched
-) : (
-    
-    <div className="min-h-screen bg-gray-100 text-gray-900">
-      <Navbar />
-      <div className="container mx-auto p-4 max-w-4xl">
-        <h1 className="text-2xl font-bold mb-4 text-gray-900">
-          {questionData.title}
-        </h1>
+      <LoadingComponent /> // Show loading screen while data is being fetched
+    ) : (
 
-        <div className="space-y-6">
-
-          <QuestionDetail
-            inputType="Question"
-            explanation={questionData.details}
-            code={questionData.code_snippet}
-            author={questionData.author}
-            language={questionData.language}
-            tags={questionData.tags}
-            initialVotes={questionData.upvote_count}
-            question_id={question_id}
-            isAnswered= {isAnswered}
-
-          />
-          {questionData.code_snippet && (
-            <button
-              className="bg-blue-600 text-white px-4 py-2 mt-4"
-              onClick={() => run_code('question', questionData.id)}
-            >
-              Run Code
-            </button>
-          )}
-        </div>
-
-        <Separator className="my-8 bg-gray-300" />
-
+      <div className="min-h-screen bg-gray-100 text-gray-900">
+        <Navbar />
         <div className="container mx-auto p-4 max-w-4xl">
+          <h1 className="text-2xl font-bold mb-4 text-gray-900">
+            {questionData.title}
+          </h1>
+
           <div className="space-y-6">
-            <h2 className="text-xl font-semibold text-gray-800">Answers</h2>
-            {commentData.length > 0 ? (
-              commentData.map((comment, index) => (
-                <React.Fragment key={index}>
-                  <Comment
-                    question_id={question_id}
-                    number={index + 1}
-                    explanation={comment.details}
-                    code={comment.code_snippet}
-                    author={comment.user}
-                    questionAuthor = {questionData.author}
-                    initialVotes={comment.upvotes}
-                    comment_id={comment.comment_id}
-                    answer_of_the_question = {comment.answer_of_the_question}
-                    fetchComments = {fetchComments}
-                  />
-                  {comment.code_snippet && (
-                    <button
-                      className="bg-blue-600 text-white px-4 py-2 mt-4"
-                      onClick={() => run_code('comment', comment.comment_id)}
-                    >
-                      Run Code
-                    </button>
-                  )}
-                </React.Fragment>
-              ))
-            ) : (
-              <p className="text-gray-600">No comments available.</p>
+
+            <QuestionDetail
+              inputType="Question"
+              explanation={questionData.details}
+              code={questionData.code_snippet}
+              author={questionData.author}
+              language={questionData.language}
+              tags={questionData.tags}
+              initialVotes={questionData.upvote_count}
+              question_id={question_id}
+              isAnswered={isAnswered}
+
+            />
+            {questionData.code_snippet && (
+              <button
+                className="bg-blue-600 text-white px-4 py-2 mt-4"
+                onClick={() => run_code('question', questionData.id)}
+              >
+                Run Code
+              </button>
             )}
-            <button className="floating-button" onClick={() => {}}>
-              +
-            </button>
-            <PostComment
-             question_id = {question_id}
-             fetchComments = {fetchComments}
-             />
           </div>
 
-          <div className="my-8">
-            <h2 className="text-xl font-semibold text-gray-800">Try Your Own Code</h2>
-            <form onSubmit={handleSubmit}>
-              <label className="block mb-2">Select Language:</label>
-              <select
-                value={languageId}
-                onChange={(e) => setLanguageId(e.target.value)}
-                className="w-full p-2 mb-4 bg-gray-200 text-gray-800"
-              >
-                <option value="">Select a language</option>
-                {Object.entries(languages).map(([name, id]) => (
-                  <option key={id} value={id}>
-                    {name}
-                  </option>
-                ))}
-              </select>
+          <Separator className="my-8 bg-gray-300" />
 
-              <label className="block mb-2">Enter your code:</label>
-              <textarea
-                value={code}
-                onChange={(e) => setCode(e.target.value)}
-                rows="10"
-                className="w-full p-2 mb-4 bg-gray-200 text-gray-800"
-              ></textarea>
-
-              <SyntaxHighlighter language="javascript" style={docco}>
-                {code}
-              </SyntaxHighlighter>
-
-              <button
-                type="submit"
-                className="bg-blue-600 text-white px-4 py-2"
-                disabled={loading || !languageId}
-              >
-                {loading ? "Executing..." : "Execute Code"}
+          <div className="container mx-auto p-4 max-w-4xl">
+            <div className="space-y-6">
+              <h2 className="text-xl font-semibold text-gray-800">Answers</h2>
+              {commentData.length > 0 ? (
+                commentData.map((comment, index) => (
+                  <React.Fragment key={index}>
+                    <Comment
+                      question_id={question_id}
+                      number={index + 1}
+                      explanation={comment.details}
+                      code={comment.code_snippet}
+                      author={comment.user}
+                      questionAuthor={questionData.author}
+                      initialVotes={comment.upvotes}
+                      comment_id={comment.comment_id}
+                      answer_of_the_question={comment.answer_of_the_question}
+                      fetchComments={fetchComments}
+                    />
+                    {comment.code_snippet && (
+                      <button
+                        className="bg-blue-600 text-white px-4 py-2 mt-4"
+                        onClick={() => run_code('comment', comment.comment_id)}
+                      >
+                        Run Code
+                      </button>
+                    )}
+                  </React.Fragment>
+                ))
+              ) : (
+                <p className="text-gray-600">No comments available.</p>
+              )}
+              <button className="floating-button" onClick={() => openPopup()}>
+                +
               </button>
-            </form>
+                {isPopupVisible && (
+                  <div className="popup" >
+                    <div className="popup-content" ref={popupRef}>
+                      <PostComment
+                        question_id={question_id}
+                        fetchComments={fetchComments}
+                        closePopup={closePopup}
+                      />
+                    </div>
+                  </div>
+                )}
+            </div>
 
-            <div className="mt-6">
-              <h2 className="text-xl font-semibold mb-2">Output:</h2>
-              <pre className="w-full p-4 bg-gray-200 text-gray-800 whitespace-pre-wrap">
-                {loading ? "Waiting for output..." : output || "No output yet."}
-              </pre>
+            <div className="my-8">
+              <h2 className="text-xl font-semibold text-gray-800">Try Your Own Code</h2>
+              <form onSubmit={handleSubmit}>
+                <label className="block mb-2">Select Language:</label>
+                <select
+                  value={languageId}
+                  onChange={(e) => setLanguageId(e.target.value)}
+                  className="w-full p-2 mb-4 bg-gray-200 text-gray-800"
+                >
+                  <option value="">Select a language</option>
+                  {Object.entries(languages).map(([name, id]) => (
+                    <option key={id} value={id}>
+                      {name}
+                    </option>
+                  ))}
+                </select>
+
+                <label className="block mb-2">Enter your code:</label>
+                <textarea
+                  value={code}
+                  onChange={(e) => setCode(e.target.value)}
+                  rows="10"
+                  className="w-full p-2 mb-4 bg-gray-200 text-gray-800"
+                ></textarea>
+
+                <SyntaxHighlighter language="javascript" style={docco}>
+                  {code}
+                </SyntaxHighlighter>
+
+                <button
+                  type="submit"
+                  className="bg-blue-600 text-white px-4 py-2"
+                  disabled={loading || !languageId}
+                >
+                  {loading ? "Executing..." : "Execute Code"}
+                </button>
+              </form>
+
+              <div className="mt-6">
+                <h2 className="text-xl font-semibold mb-2">Output:</h2>
+                <pre className="w-full p-4 bg-gray-200 text-gray-800 whitespace-pre-wrap">
+                  {loading ? "Waiting for output..." : output || "No output yet."}
+                </pre>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    
-)}
-</div>
+
+    )}
+  </div>
   );
 }
